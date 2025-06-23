805e7e028b30b22fa1dc6cd4c7c01ce100a2be06
princeVillamil <jeffreyvillamilwork@gmail.com>
2025-06-23 23:00:11 +0800

    add .env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1S6-DKAeCCxwlKpKlm1vuFpIUize74lQ",
-  authDomain: "aucmaservicemanagement.firebaseapp.com",
-  projectId: "aucmaservicemanagement",
-  storageBucket: "aucmaservicemanagement.firebasestorage.app",
-  messagingSenderId: "391005941352",
-  appId: "1:391005941352:web:9a51fd1546472ce29515dd",
-  measurementId: "G-2S4F1Y1HP2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
