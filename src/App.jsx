51e0f749a90d270e72253ac24ce241752b658c3a
princeVillamil <jeffreyvillamilwork@gmail.com>
2025-06-26 09:59:58 +0800

    trying new UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
 
-import { Button, Card, Typography } from "@material-tailwind/react";
+import React from 'react'
+import HomePage from './pages/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <Card className="p-6 shadow-lg">
-        <Typography variant="h4" color="blue-gray">
-          Material Tailwind + Vite
-        </Typography>
-        <Button className="mt-4">Click Me</Button>
-      </Card>
-    </div>
+    <>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
     
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './firebase/authContext.jsx'
+import { Router, Routes } from 'react-router-dom';
+// import { AuthProvider } from './firebase/authContext.jsx'
 import 'leaflet/dist/leaflet.css';
 import './index.css'
 
 //Pages
 import App from './App.jsx'
-import LoginPage from './pages/LoginPage.jsx'
-import RegisterPage from './pages/RegisterPage.jsx';
-import HomePage from './pages/HomePage.jsx';
-import AdminPage from './pages/AdminPage.jsx';
-import TestimonialPage from './pages/TestimonialPage.jsx'
-import ClientPage from './pages/ClientPage.jsx';
-
-import TestPage from './pages/TestPage.jsx'
 
 import { ThemeProvider } from "@material-tailwind/react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
+    {/* <AuthProvider> */}
     <ThemeProvider>
       <Router>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/testimonials" element={<TestimonialPage />} />
-          <Route path="/form" element={<ClientPage />} />
-
-          {/* Might need to add only Admin access */}
-          <Route path="/admin" element={<TestPage />} />
-          {/* <Route path="/test" element={<TestPage />} /> */}
-
+          <App/>
         </Routes>
       </Router>
     </ThemeProvider>
-    </AuthProvider>
+    {/* </AuthProvider> */}
   </StrictMode>,
 )
